09c2761e19c8c4be6b49c3f9ffeb751c9bd9761c
MarcoDeto <marco.detomasi@yahoo.it>
2022-07-10 00:17:56 +0200

    u

--- Er_Crypto_Bot/config.py
+++ Er_Crypto_Bot/config.py
@@ -1,5 +1,5 @@
 CONNECTION_STRING = 'mongodb+srv://dev:ManTyres@mantyres.fwdxdp6.mongodb.net'
-INTERVALS = ['5m', '15m', '30m', '1h', '4h', '1d']
+INTERVALS = ['1m', '3m', '5m', '15m', '30m', '45m', '1h', '2h', '3h', '4h', '1d']
 SECONDS_EMA = [25, 50, 60, 123]
 MAIN_EMA = 10
 api_key = 'ySBxTVFMkh3pGRyE1v8PqXXGbZoTcBs0eI2GuLvb99wjgWbQk3MiQxFjOH7SYqgC'
