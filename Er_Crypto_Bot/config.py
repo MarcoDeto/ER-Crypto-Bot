5f77ac2b9c6797c311ee7c4b0f69e1050a1d3251
Marco De Tomasi <marco.detomasi@yahoo.it>
2022-07-12 23:10:52 +0200

    ok

--- Er_Crypto_Bot/config.py
+++ Er_Crypto_Bot/config.py
@@ -5,7 +5,7 @@ MAIN_EMA = 10
 api_key = 'ySBxTVFMkh3pGRyE1v8PqXXGbZoTcBs0eI2GuLvb99wjgWbQk3MiQxFjOH7SYqgC'
 api_secret = '6l2M7hMmg9AHprlFSMLER3xMSN735ioGgH69dY801aAKUvrNQs2KgP8JLLe9QSB7'
 SYMBOLS = ['BTC']
-
+START_DATE = '17 August, 2017'
 
 #incrocio short: 10 con 123 su daily 
 #incrocio long: 10 con 60 su daily 
