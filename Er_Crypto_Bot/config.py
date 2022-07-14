d2d00ad38fd47085f2057521f1b8d89dae67d777
Marco De Tomasi <marco.detomasi@yahoo.it>
2022-07-14 23:13:31 +0200

    minor bug fixed

--- Er_Crypto_Bot/config.py
+++ Er_Crypto_Bot/config.py
@@ -1,10 +1,10 @@
 CONNECTION_STRING = 'mongodb+srv://dev:ManTyres@mantyres.fwdxdp6.mongodb.net'
-INTERVALS = ['1m', '3m', '5m', '15m', '30m', '45m', '1h', '2h', '3h', '4h', '1d']
+INTERVALS = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '1d']
 SECONDS_EMA = [25, 50, 60, 123]
 MAIN_EMA = 10
 api_key = 'ySBxTVFMkh3pGRyE1v8PqXXGbZoTcBs0eI2GuLvb99wjgWbQk3MiQxFjOH7SYqgC'
 api_secret = '6l2M7hMmg9AHprlFSMLER3xMSN735ioGgH69dY801aAKUvrNQs2KgP8JLLe9QSB7'
-SYMBOLS = ['SOL']
+SYMBOLS = ['BNB']
 START_DATE = '17 August, 2017'
 TELEGRAM_TOKEN = "5554013164:AAEaxjGrj2KHUbGuSz9MCH4aeKGcy7c28Ao"
 
